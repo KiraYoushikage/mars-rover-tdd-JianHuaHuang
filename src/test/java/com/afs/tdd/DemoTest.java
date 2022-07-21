f0c5de80237c2f0af72e88a4742263f7dc37e340
KiraYous <404465394@qq.com>
2022-07-21 17:53:36 +0800

    test:feat:goto_0_0_S_when_command_L_given_0_0_W

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -74,5 +74,15 @@ class DemoTest {
         Assertions.assertEquals(0,marsRover.getY());
         Assertions.assertEquals("S",marsRover.getDirection());
     }
-    
+    @Test
+    void should_goto_0_0_E_when_command_L_given_0_0_S() {
+        //given
+        MarsRover marsRover=new MarsRover(0,0,"S");
+        //when
+        marsRover.executeCommand("L");
+        //then
+        Assertions.assertEquals(0,marsRover.getX());
+        Assertions.assertEquals(0,marsRover.getY());
+        Assertions.assertEquals("E",marsRover.getDirection());
+    }
 }

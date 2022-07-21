005d93f331f90340df7385ca9d46992dcdd83440
KiraYous <404465394@qq.com>
2022-07-21 16:36:07 +0800

    test:should_goto_0_1_N_when_command_M_given_0_0_N

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,22 @@
+package com.afs.tdd;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MarsRover {
+    private int x;
+    private int y;
+    private String direction;
+
+    public void executeCommand(String command) {
+        if(command.equals("M")){
+            if (direction.equals("N")){
+                y++;
+            }
+
+        }
+
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -1,10 +1,20 @@
 package com.afs.tdd;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DemoTest {
-    @Test
-    void write_your_first_test() {
 
+    @Test
+    void should__when__given_() {
+        //given
+        MarsRover marsRover=new MarsRover(0,0,"N");
+        //when
+        marsRover.executeCommand("M");
+        //then
+        Assertions.assertEquals(0,marsRover.getX());
+        Assertions.assertEquals(1,marsRover.getY());
+        Assertions.assertEquals("N",marsRover.getDirection());
     }
+    
 }
